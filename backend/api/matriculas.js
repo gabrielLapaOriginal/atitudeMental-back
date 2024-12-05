7592d550a45b7c6cb280218e2305981ef184304f
gabrielLapaOriginal <gabriel.lapa2001@yahoo.com.br>
2024-12-05 11:55:04 -0300

    Correcao

---
+++ backend/api/matriculas.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const MONGO_URI = "mongodb+srv://admin:admin@cluster0.fkldq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const MatriculaSchema = new mongoose.Schema({
+  matricula: { type: String, required: true },
+}, { timestamps: true });
+
+const Matricula = mongoose.model("Matricula", MatriculaSchema);
+
+module.exports = async (req, res) => {
+  if (mongoose.connection.readyState !== 1) {
+    try {
+      await mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log("Conectado");
+    } catch (err) {
+      console.error("Erro ao conectar ao MongoDB:", err);
+      return res.status(500).json({ message: "Erro ao conectar ao banco de dados" });
+    }
+  }
+
+  if (req.method === 'POST') {
+    const { matricula } = req.body;
+    try {
+      const novaMatricula = new Matricula({ matricula });
+      await novaMatricula.save();
+      return res.status(201).json({ message: 'Matrícula salva com sucesso' });
+    } catch (error) {
+      return res.status(500).json({ message: `Erro ao salvar matrícula: ${error.message}` });
+    }
+  } else {
+    return res.status(405).json({ message: 'Método não permitido' });
+  }
+};
